61610cafd123e6251fb50504515cad46d7c7f332
Alexey Melezhik <melezhik@gmail.com>
2014-09-17 11:12:36 +0400

    migrate

---
+++ jessy/db/migrate/20140917071138_drop_logs_table.rb
@@ -0,0 +1,4 @@
+class DropLogsTable < ActiveRecord::Migration
+  def change
+  end
+end
--- jessy/db/schema.rb
+++ jessy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140916084916) do
+ActiveRecord::Schema.define(version: 20140917071138) do
 
   create_table "builds", force: true do |t|
     t.string   "state",             default: "scheduled"
