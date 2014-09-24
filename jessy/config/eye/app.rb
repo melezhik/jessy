be5fa33e11734f5841db3b2e7ae13d40f54a34ee
Alexey Melezhik <melezhik@gmail.com>
2014-09-24 11:54:02 +0400

    eye config for dj

--- jessy/config/eye/app.rb
+++ jessy/config/eye/app.rb
@@ -15,16 +15,12 @@ Eye.application app do
 
     group 'dj' do
 
-        chain :action => :restart, :grace => 5.seconds
-        chain :action => :start, :grace => 0.2.seconds
-    
         workers = (ENV['dj_workers']||'2').to_i
         (1..workers).each do |i|
             process "dj#{i}" do
                 pid_file "tmp/pids/delayed_job.#{i}.pid" # pid_path will be expanded with the working_dir
-                start_command "./bin/delayed_job start -i #{i}"
-                stop_command "./bin/delayed_job stop -i #{i}"
-                daemonize false
+                start_command "rake jobs:work"
+                daemonize true
                 stdall "#{cwd}/log/dj.eye.log"
                 env 'RESTCLIENT_LOG' => "#{cwd}/log/rc.log"
                 env 'PINTO_REPOSITORY_ROOT' =>  ENV['HOME'] + '/.jessy/repo/'
